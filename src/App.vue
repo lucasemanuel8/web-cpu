2a40210db93ae1fcb98bff987d8630cb262b332b
Lucas Emanuel <lucaxemanuel@gmail.com>
2019-12-10 14:50:44 -0300

    Adicionando o Bootstrap

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,27 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="container" id="app">
+    <Instruction/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Instruction from './components/Instruction.vue';
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    Instruction
   }
 }
 </script>
 
 <style>
+body {
+  background: #0F2027;  /* fallback for old browsers */
+  background: -webkit-linear-gradient(to left, #2C5364, #203A43, #0F2027);  /* Chrome 10-25, Safari 5.1-6 */
+  background: linear-gradient(to left, #2C5364, #203A43, #0F2027); /* W3C, IE 10+/ Edge, Firefox 16+, Chrome 26+, Opera 12+, Safari 7+ */
+}
 input {
   border:none;
   background-image:none;
@@ -29,8 +34,5 @@ input {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #ffffff;
-  background: #23074d;  /* fallback for old browsers */
-  background: -webkit-linear-gradient(to right, #cc5333, #23074d);  /* Chrome 10-25, Safari 5.1-6 */
-  background: linear-gradient(to right, #cc5333, #23074d); /* W3C, IE 10+/ Edge, Firefox 16+, Chrome 26+, Opera 12+, Safari 7+ */
 }
 </style>
