2c45f1547eef91b45d0b59786e80ed38b315dbfa
Lucas Emanuel <lucaxemanuel@gmail.com>
2019-12-11 09:57:52 -0300

    Modificando background

--- src/App.vue
+++ src/App.vue
@@ -19,9 +19,10 @@ export default {
 <style>
 @import url('https://fonts.googleapis.com/css?family=Lato|Lilita+One&display=swap');
 body {
-  background: #360b2f;  /* fallback for old browsers */
-  background: url("https://www.transparenttextures.com/patterns/cubes.png"), -webkit-linear-gradient(to left, #c31432, #360b2f); /* Chrome 10-25, Safari 5.1-6 */
-  background: url("https://www.transparenttextures.com/patterns/cubes.png"), linear-gradient(to left, #c31432, #360b2f); /* W3C, IE 10+/ Edge, Firefox 16+, Chrome 26+, Opera 12+, Safari 7+ */
+
+  background: #ED213A;  /* fallback for old browsers */
+  background: url("https://www.transparenttextures.com/patterns/cubes.png"), -webkit-linear-gradient(to right, rgb(138, 6, 6), #ED213A);  /* Chrome 10-25, Safari 5.1-6 */
+  background: url("https://www.transparenttextures.com/patterns/cubes.png"), linear-gradient(to right, rgb(138, 6, 6), #ED213A); /* W3C, IE 10+/ Edge, Firefox 16+, Chrome 26+, Opera 12+, Safari 7+ */
 }
 
 input {
